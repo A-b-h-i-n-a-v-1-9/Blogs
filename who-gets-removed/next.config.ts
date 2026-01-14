75bc23bb490df979200dad5f3d1d2a1b6cdb17fa
Abhinav <abhinavmehta374@gmail.com>
2026-01-14 15:21:09 +0530

    without demo

--- who-gets-removed/next.config.ts
+++ who-gets-removed/next.config.ts
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-
   basePath: "/blogs/who-gets-removed",
-  assetPrefix: "/blogs/who-gets-removed",
-
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
