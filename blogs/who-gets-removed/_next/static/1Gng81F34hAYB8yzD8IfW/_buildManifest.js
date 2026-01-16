0e3187b2ba399fd2c4dd782a8aed5d7ddfdb8c33
Abhinav <abhinavmehta374@gmail.com>
2026-01-16 11:27:15 +0530

    Updates

--- blogs/who-gets-removed/_next/static/1Gng81F34hAYB8yzD8IfW/_buildManifest.js
+++ blogs/who-gets-removed/_next/static/1Gng81F34hAYB8yzD8IfW/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Blogs/who-gets-removed//_next/:path+",
+        "destination": "/Blogs/who-gets-removed/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
